14eb56e8476410e153168f5faf798b8a11d7384f
369Dawi369 <david.erwin.cz68@gmail.com>
2025-11-15 15:35:07 +0100

    front end arck

---
+++ frontend/src/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// export const POLYGON_API_KEY = process.env.POLYGON_API_KEY || "no_api_key_:(";
+
+// export const HUB_REST_PORT = process.env.HUB_REST_PORT
+//   ? parseInt(process.env.HUB_REST_PORT)
+//   : 3000;
